5de5b05be1eae9e23beb45509755a654401399fd
Sunny Patel <sunny.patel@monash.edu>
2022-04-21 16:40:28 +1000

    Added and improvised container variabler

--- terraform/terraform_webapp/container_definitions.json.variables.tf
+++
@@ -1,5 +0,0 @@
-variable "container_definitions__dbhost" {}
-variable "container_definitions__postgres_user" {default = "postgres" }
-variable "container_definitions__postgres_password" {}
-variable "container_definitions__postgres_password" {}
-variable "container_definitions__postgres_password" {}
--- terraform/terraform_webapp/variables.tf
+++ terraform/terraform_webapp/variables.tf
@@ -9,4 +9,19 @@ variable "cluster_name" {
   default = "serviancluster"
 }
 
+variable "container_definitions__dbhost" {
+  type = string
+  description = "The name of the RDS DB Host"
+}
+variable "container_definitions__postgres_user" {
+  type = string
+  description = "The name of the RDS DB username"
+  default = "postgres" 
+  }
+variable "container_definitions__postgres_password" {
+  type = string
+  description = "The name of the RDS DB password"
+}
+
+
 # -var="key_name=id_rsa" -var="cluster_name=test_cluster"
